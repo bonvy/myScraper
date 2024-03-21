8dd167ceb9042265f8f5d20d17c84221468cac4e
bonvy <49160177+bonvy@users.noreply.github.com>
2024-03-21 14:09:11 +0100

    up

--- main.js
+++ main.js
@@ -1,7 +1,8 @@
 import fetch from "node-fetch";
 import cheerio from "cheerio";
-import fs from 'fs';
+
 import pkg from 'pg';
+
 const { Client } = pkg
 // Configura le credenziali di accesso al database
 const client = new Client({
@@ -12,92 +13,97 @@ const client = new Client({
     port: 5432 // Porta di default di PostgreSQL
 });
 
-const loadUrl = JSON.parse(fs.readFileSync('./url.json', { encoding: 'utf8' }));
+
 const date = new Date()
-client.connect()
-    .then(() => console.log('Connesso al database PostgreSQL!'))
 
-loadUrl.url.forEach(async function (url) {
+client.connect().then(() => console.log('Connesso al database PostgreSQL!'))
+let loadUrl=[];
+
+
+await client.query('SELECT url, date_sc FROM "URLS"').then(resp => resp.rows.forEach(el => loadUrl.push({url: el.url, date_sc: el.date_sc})));
+let sc=loadUrl.length;
+await scraperStart();
+   
+async function scraperStart() {
+
+    loadUrl.forEach(async function (obj) {
+        if(obj?.date_sc?.getDay()===date.getDay() && obj?.date_sc?.getMonth()===date.getMonth() && !obj.date){
+            fetch(obj.url, {
+                "headers": {
+                    "accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7",
+                    "accept-language": "en-US,en;q=0.9,it-IT;q=0.8,it;q=0.7",
+                    "cache-control": "max-age=0",
+                    "sec-ch-ua": "\"Chromium\";v=\"122\", \"Not(A:Brand\";v=\"24\", \"Google Chrome\";v=\"122\"",
+                    "sec-ch-ua-mobile": "?0",
+                    "sec-ch-ua-platform": "\"macOS\"",
+                    "sec-fetch-dest": "document",
+                    "sec-fetch-mode": "navigate",
+                    "sec-fetch-site": "same-origin",
+                    "sec-fetch-user": "?1",
+                    "upgrade-insecure-requests": "1"
+                },
+                "referrer": "https://www.sportler.com/it/l/pantaloni_sci_alpinismo-uomo?itm_source=menu&itm_medium=link&itm_campaign=uomo/pantaloni/pantaloni-scialpinismo",
+                "referrerPolicy": "strict-origin-when-cross-origin",
+                "body": null,
+                "method": "GET",
+                "mode": "cors",
+                "credentials": "include"
+            }).then((data) => {
+                try {
+                    data.text().then((html) => {
     
-    fetch(url, {
-        "headers": {
-            "accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7",
-            "accept-language": "en-US,en;q=0.9,it-IT;q=0.8,it;q=0.7",
-            "cache-control": "max-age=0",
-            "sec-ch-ua": "\"Chromium\";v=\"122\", \"Not(A:Brand\";v=\"24\", \"Google Chrome\";v=\"122\"",
-            "sec-ch-ua-mobile": "?0",
-            "sec-ch-ua-platform": "\"macOS\"",
-            "sec-fetch-dest": "document",
-            "sec-fetch-mode": "navigate",
-            "sec-fetch-site": "same-origin",
-            "sec-fetch-user": "?1",
-            "upgrade-insecure-requests": "1"
-        },
-        "referrer": "https://www.sportler.com/it/l/pantaloni_sci_alpinismo-uomo?itm_source=menu&itm_medium=link&itm_campaign=uomo/pantaloni/pantaloni-scialpinismo",
-        "referrerPolicy": "strict-origin-when-cross-origin",
-        "body": null,
-        "method": "GET",
-        "mode": "cors",
-        "credentials": "include"
-
-    }).then((data) => {
-        try{
-        data.text().then((html) => {
-           
-            const $ = cheerio.load(html)
-            const scriptText = $('script._iub_cs_activate').text();
-
-            // Trova il valore della chiave 'value'
-            const match = scriptText.match(/value:\s*([\d.]+)/);
-
-            var oggetto = {}
-
-            
-
-            // Trova il prezzo attuale
-            const currentPrice = $('.prodDetailPrice .singleProductPrice span[itemprop="price"]').text().trim().replace(',', '.');
-
-            const isDiscounted = $('.singleProductPrice .bullet-rect').length > 0;
-            // Trova il prezzo precedente
-            const previousPrice = $('.singleProductPrice .prevPrice').text().trim().replace('€', '').replace(',', '.');
-            oggetto.prezzoAttuale = parseFloat(currentPrice);
-            if (isDiscounted) {
-                oggetto.prezzoIntero = parseFloat(previousPrice);
-            } else {
-                oggetto.prezzoIntero = parseFloat(currentPrice);
-            }
-            // Trova la tag title e ne estrae il testo
-            const title = $('title').text().trim();
-
-            // Estrai il testo prima del carattere "-"
-            const name = title.split(/\s+-\s+/)[0].trim();
-
-
-
-            oggetto.codice = $('#AjaxValueVariationSkuTemplate').text().trim();
-            try {
-                if(oggetto.codice){
-                    client.query("SELECT EXISTS (SELECT 1 FROM product WHERE sku = '"+oggetto.codice+"' AND date_sc = '"+ date.toISOString().slice(0, 10)+"') AS entita_esiste;").then((data) => {
-                        !data.rows[0].entita_esiste ?  client.query('INSERT INTO product (price, pricescont ,sku, date_sc, name,store) VALUES ($1,$2,$3,$4,$5,$6)', [oggetto.prezzoIntero, oggetto.prezzoAttuale, oggetto.codice, date,name,"https://www.sportler.com/it"]) : null
-                    })
-                   
-                }
-               
-            } catch (e) {
-
-                console.log(e)
-            }
-        });}catch(e){}
+                        const $ = cheerio.load(html);
+                        const scriptText = $('script._iub_cs_activate').text();
+    
+                        // Trova il valore della chiave 'value'
+                        const match = scriptText.match(/value:\s*([\d.]+)/);
+    
+                        var oggetto = {};
+                       
+    
+                        // Trova il prezzo attuale
+                        const currentPrice = $('.prodDetailPrice .singleProductPrice span[itemprop="price"]').text().trim().replace(',', '.');
+    
+                        const isDiscounted = $('.singleProductPrice .bullet-rect').length > 0;
+                        // Trova il prezzo precedente
+                        const previousPrice = $('.singleProductPrice .prevPrice').text().trim().replace('€', '').replace(',', '.');
+                        oggetto.prezzoAttuale = parseFloat(currentPrice);
+                        if (isDiscounted) {
+                            oggetto.prezzoIntero = parseFloat(previousPrice);
+                        } else {
+                            oggetto.prezzoIntero = parseFloat(currentPrice);
+                        }
+                        // Trova la tag title e ne estrae il testo
+                        const title = $('title').text().trim();
+    
+                        // Estrai il testo prima del carattere "-"
+                        const name = title.split(/\s+-\s+/)[0].trim();
+    
+    
+    
+                        oggetto.codice = $('#AjaxValueVariationSkuTemplate').text().trim();
+                        try {
+                            if (oggetto.codice) {
+                                client.query("SELECT EXISTS (SELECT 1 FROM product WHERE sku = '" + oggetto.codice + "' AND date_sc = '" + date.toISOString().slice(0, 10) + "') AS entita_esiste;").then((data) => {sc--
+                                    !data.rows[0].entita_esiste ? client.query('INSERT INTO product (price, pricescont ,sku, date_sc, name,store) VALUES ($1,$2,$3,$4,$5,$6)', [oggetto.prezzoIntero, oggetto.prezzoAttuale, oggetto.codice, date, name, "https://www.sportler.com/it"]) : null;
+                                    client.query('SELECT * FROM "URLS" WHERE url=$1',[obj.url]).then((resp) => resp.rows.length>=1?  client.query('UPDATE "URLS" SET date_sc=$2 WHERE url=$1', [obj.url,date.toISOString()]): null  )
+                                });
+    
+                            }else{
+                                sc--;
+                            }
+    
+                        } catch (e) {
+    
+                            console.log(e);
+                        }
+                    });
+                } catch (e) { }
+    
+            }); 
+        }else{
+            sc--}
 
     });
-
-
-
-
-});
-
-
-
-
-
-
+}
+setInterval(()=>{sc===0 ?  process.exit(): console.log(sc) },500)
--- urlSaver.js
+++ urlSaver.js
@@ -1,31 +1,54 @@
 import puppeteer from 'puppeteer';
-import fs from 'fs'
+import CryptoJS from 'crypto-js';
+import pkg from 'pg';
+const { Client } = pkg
+// Configura le credenziali di accesso al database
+const client = new Client({
+    user: 'postgres', // Nome utente del database
+    host: 'localhost', // Indirizzo del server PostgreSQL
+    database: 'postgres', // Nome del database
+    password: 'root', // Password del database
+    port: 5432 // Porta di default di PostgreSQL
+});
+let sc;
+client.connect()
+    .then(() => console.log('Connesso al database PostgreSQL!'));
 (async () => {
     // Avvia una nuova istanza di browser
     const browser = await puppeteer.launch();
-  
+
     // Apre una nuova pagina
     const page = await browser.newPage();
-  
+
     // Naviga verso il sito web desiderato
-    await page.goto('https://www.sportler.com/it/l/pantaloni_softshell-donna?itm_source=menu&itm_medium=link&itm_campaign=donna/pantaloni-e-gonne/pantaloni-softshell');
-  
+    await page.goto('https://www.sportler.com/it/l/calzini-donna?open=filterSports&itm_source=menu&itm_medium=link&itm_campaign=donna/intimo-e-accessori/calzini');
+ 
     var urls = await page.evaluate(() => {
         const elements = document.querySelectorAll('div.singleProductBoxImg a.activeVariant');
         const urls = Array.from(elements).map(element => {
-          return 'https://www.sportler.com' + element.getAttribute('href');
+            return 'https://www.sportler.com' + element.getAttribute('href').trim();
         });
         return urls;
-      });
-      urls=[... new Set(urls)]
-      // Stampa gli URL
-      const data = { "url": urls };
-
-      // Convertiamo l'oggetto JSON in una stringa
-      const jsonData = JSON.stringify(data, null, 2);
-    
-      // Salva gli URL in un file JSON
-      fs.appendFileSync('url.json', jsonData);
+    });
+    urls = [... new Set(urls)]
+    // Stampa gli URL
+    sc=urls.length;
+    urls.forEach(url => {
+        const hashValue = CryptoJS.SHA256(rimuoviSpaziBianchi(url))
+       
+        client.query('SELECT COUNT(url) AS numb FROM "URLS" WHERE url=$1',[url]).then((resp) => resp.rows[0].numb!=='1' ?  client.query('INSERT INTO "URLS" (url) VALUES($1)', [rimuoviSpaziBianchi(url)]): null,sc--)
+       
+    });
     // Chiude il browser
+    setInterval(()=> sc===0? process.exit():null )
     await browser.close();
-  })();
+})();
+
+
+
+function rimuoviSpaziBianchi(stringa) {
+    // Utilizziamo una espressione regolare per sostituire tutti gli spazi bianchi con una stringa vuota
+    let str= stringa.replace(/\s+/g, '');
+    return str.replace(/\t/g,'');
+}
+
